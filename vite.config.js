6da364e5a187c4d1cec1c7872dc15de579248364
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2023-08-08 13:11:57 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/peace-and-love/',
+  base: '/open-spot-web/',
   test: {
     environment: 'happy-dom',
   },
