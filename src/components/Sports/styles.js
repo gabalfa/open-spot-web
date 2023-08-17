e7f303052d2a290cc4fd5f2f61cfae255cc1d339
Gabriel Alfonso <gabrielalfonso@Gabriels-MacBook-Pro.local>
2023-08-16 23:28:20 -0500

    adding forecast feature, tryinng correct way to casting dates with local time

--- src/components/Sports/styles.js
+++ src/components/Sports/styles.js
@@ -108,7 +108,6 @@ export const ForecastContainer = styled.div`
   /* border-bottom: 1px solid ${TEXT_COLORS.BLUE_OWN}; */
   /* margin-left: 45px; */
   width: 100%;
-  margin-top: 6px;
 `
 
 export const InnerForecastContainer = styled.div`
